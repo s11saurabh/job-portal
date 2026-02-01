2ddbcb9f5b865deea202fa4f8185ef45232a53aa
SAURABH SINGHANIA <saurabhsinghania111@gmail.com>
2026-02-01 17:25:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,31 +11,35 @@ import connectCloudinary from './config/cloudinary.js';
 import JobRoutes from './routes/jobRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
-// Initialize Express
+
 const app = express();
 
-// Connect to MongoDB
 await connectDB();
 await connectCloudinary();
 
-// Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://job-portal-ordg.vercel.app',
+    'http://localhost:5173',
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(clerkMiddleware());
 
-// Routes
 app.get("/", (req, res) => res.send("API Working"));
+
+app.post('/webhooks', clerkWebhooks)
+app.use('/api/company', companyRoutes)
+app.use('/api/jobs', JobRoutes)
+app.use('/api/users', userRoutes)
 
 Sentry.setupExpressErrorHandler(app);
 
 app.get("/debug-sentry", function mainHandler(req, res) {
-    throw new Error("My first Sentry error!");
-  });
-app.post('/webhooks',clerkWebhooks)
-app.use('/api/company',companyRoutes)
-app.use('/api/jobs', JobRoutes)
-app.use('/api/users', userRoutes)
+  throw new Error("My first Sentry error!");
+});
 
-// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
