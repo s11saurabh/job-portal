661c241f6119bb2aa487fed5a2f772d16608f394
SAURABH SINGHANIA <saurabhsinghania111@gmail.com>
2026-02-01 17:39:11 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected',() => console.log('Database Connected'))
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+    mongoose.connection.on('connected', () => console.log('Database Connected'))
+    const baseUri = process.env.MONGODB_URI.replace(/\/+$/, '');
+    await mongoose.connect(`${baseUri}/job-portal`)
 }
 
 export default connectDB;
